ceee37f3682e9984719e515ae58ac3b5c470fd33
Olga Kislova <gavrina0910@gmail.com>
2022-12-27 22:38:11 +0300

    add title label

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     private let avatarIView = AvatarImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    private let titleLView = TitleLabelView(frame: CGRect(x: 0, y: 0, width: 150, height: 18))
     
     private var navigationBarHeight: CGFloat { self.safeAreaInsets.top }
     
@@ -18,9 +19,22 @@ class ProfileHeaderView: UIView {
     
     public func configure() {
         self.addSubview(avatarIView)
+        self.addSubview(titleLView)
     }
 
     public func recalculationPosition() {
-        avatarIView.frame = CGRect(x: 16, y: navigationBarHeight + 16, width: avatarIView.frame.width, height: avatarIView.frame.height)
+        avatarIView.frame = CGRect(
+            x: 16,
+            y: navigationBarHeight + 16,
+            width: avatarIView.frame.width,
+            height: avatarIView.frame.height
+        )
+        
+        titleLView.frame = CGRect(
+            x: avatarIView.frame.maxX + 16,
+            y: navigationBarHeight + 27,
+            width: titleLView.frame.width,
+            height: titleLView.frame.height
+        )
     }
 }
